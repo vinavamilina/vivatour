52dd41f5a28ad696f935c36e852cb9eebafb665d
richoandika <richoandika@null.net>
2021-02-02 18:49:43 +0700

    Perubahan ke-3

--- database/vivanew.sql
+++ database/vivanew.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 01, 2021 at 07:20 AM
+-- Generation Time: Feb 02, 2021 at 12:48 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -25,25 +25,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `hak_akses`
---
-
-CREATE TABLE `hak_akses` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `hak_akses`
---
-
-INSERT INTO `hak_akses` (`id`, `nama`) VALUES
-(1, 'Admin'),
-(2, 'Pelanggan');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `paket`
 --
 
@@ -52,7 +33,8 @@ CREATE TABLE `paket` (
   `nama` varchar(255) NOT NULL,
   `paket_kategori_id` int(11) NOT NULL,
   `paket_wisata_id` int(11) NOT NULL,
-  `durasi` char(50) NOT NULL,
+  `malam` int(11) NOT NULL,
+  `hari` int(11) NOT NULL,
   `harga` char(255) NOT NULL,
   `keterangan` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -61,97 +43,12 @@ CREATE TABLE `paket` (
 -- Dumping data for table `paket`
 --
 
-INSERT INTO `paket` (`id`, `nama`, `paket_kategori_id`, `paket_wisata_id`, `durasi`, `harga`, `keterangan`) VALUES
-(2, 'Paket Hemat Bali', 1, 1, '2', '20000000000', '-');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `paket_kategori`
---
-
-CREATE TABLE `paket_kategori` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `paket_kategori`
---
-
-INSERT INTO `paket_kategori` (`id`, `nama`) VALUES
-(1, 'Tipe A'),
-(2, 'Tipe B'),
-(3, 'Tipe C');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `paket_wisata`
---
-
-CREATE TABLE `paket_wisata` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `keterangan` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `paket_wisata`
---
-
-INSERT INTO `paket_wisata` (`id`, `nama`, `keterangan`) VALUES
-(1, 'Bali, Indonesia', '-'),
-(2, 'Kuala Lumpur, Malaysia', '-');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pengguna`
---
-
-CREATE TABLE `pengguna` (
-  `id` int(11) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `nama` varchar(150) NOT NULL,
-  `alamat` varchar(255) NOT NULL,
-  `kota` char(50) NOT NULL,
-  `hak_akses_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `pengguna`
---
-
-INSERT INTO `pengguna` (`id`, `username`, `password`, `nama`, `alamat`, `kota`, `hak_akses_id`) VALUES
-(1, 'admin', 'c4ca4238a0b923820dcc509a6f75849b', 'Billie Kohler', '276 Dominique Stravenue', 'Axelville', 1),
-(2, 'user', 'c4ca4238a0b923820dcc509a6f75849b', 'Gayle Koch', '49106 Glover Skyway', 'Shanellebury', 2),
-(3, 'user2', 'c4ca4238a0b923820dcc509a6f75849b', 'Juana Kreiger', '44320 McKenzie Ville', 'Mrazshire', 2);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `transaksi`
---
-
-CREATE TABLE `transaksi` (
-  `id` int(11) NOT NULL,
-  `paket_id` int(11) NOT NULL,
-  `pengguna_id` int(11) NOT NULL,
-  `jumlah` int(11) NOT NULL,
-  `status` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `paket` (`id`, `nama`, `paket_kategori_id`, `paket_wisata_id`, `malam`, `hari`, `harga`, `keterangan`) VALUES
+(10, 'Dummy 1', 1, 1, 3, 2, '10000000', 'blablabla\r\n\r\n-blabla\r\n-blabla');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `hak_akses`
---
-ALTER TABLE `hak_akses`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `paket`
@@ -162,65 +59,14 @@ ALTER TABLE `paket`
   ADD KEY `wisata_id` (`paket_wisata_id`);
 
 --
--- Indexes for table `paket_kategori`
---
-ALTER TABLE `paket_kategori`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `paket_wisata`
---
-ALTER TABLE `paket_wisata`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `pengguna`
---
-ALTER TABLE `pengguna`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `hak_akses_id` (`hak_akses_id`);
-
---
--- Indexes for table `transaksi`
---
-ALTER TABLE `transaksi`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `paket_id` (`paket_id`),
-  ADD KEY `pelanggan_id` (`pengguna_id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `hak_akses`
---
-ALTER TABLE `hak_akses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `paket`
 --
 ALTER TABLE `paket`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `paket_kategori`
---
-ALTER TABLE `paket_kategori`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `paket_wisata`
---
-ALTER TABLE `paket_wisata`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `pengguna`
---
-ALTER TABLE `pengguna`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
@@ -230,21 +76,8 @@ ALTER TABLE `pengguna`
 -- Constraints for table `paket`
 --
 ALTER TABLE `paket`
-  ADD CONSTRAINT `paket_ibfk_1` FOREIGN KEY (`paket_kategori_id`) REFERENCES `paket_kategori` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `paket_ibfk_2` FOREIGN KEY (`paket_wisata_id`) REFERENCES `paket_wisata` (`id`) ON UPDATE CASCADE;
-
---
--- Constraints for table `pengguna`
---
-ALTER TABLE `pengguna`
-  ADD CONSTRAINT `pengguna_ibfk_1` FOREIGN KEY (`hak_akses_id`) REFERENCES `hak_akses` (`id`) ON UPDATE CASCADE;
-
---
--- Constraints for table `transaksi`
---
-ALTER TABLE `transaksi`
-  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`paket_id`) REFERENCES `paket` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`pengguna_id`) REFERENCES `pengguna` (`id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `paket_ibfk_1` FOREIGN KEY (`paket_kategori_id`) REFERENCES `paket_kategori` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `paket_ibfk_2` FOREIGN KEY (`paket_wisata_id`) REFERENCES `paket_wisata` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
